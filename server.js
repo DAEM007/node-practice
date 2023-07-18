6609289e89afb70b1b2b057a5e3671f43d79b0fe
Emmanuel <dammylola299@gmail.com>
2023-07-18 13:07:07 +0100

    updates

--- server.js
+++ server.js
@@ -8,8 +8,22 @@ const server = http.createServer((req, res) => {
     // set header content-type
     res.setHeader('content-type', 'text/html');
 
+    // create a simple routing system
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
     // send an html file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.log(err.message);
             res.end();
