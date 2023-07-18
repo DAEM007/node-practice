7f5783db3c9c447ca5d6372aebba3c401def6f7e
Emmanuel <dammylola299@gmail.com>
2023-07-18 12:16:06 +0100

    updates

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content-type
+    res.setHeader('content-type', 'text/html');
+
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log(err.message);
+            res.end();
+        }
+        res.write(data);
+        res.end;
+    });
+
 });
 
 server.listen(3000, 'localhost', () => {
