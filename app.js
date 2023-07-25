c0ac05abf553e321bf544df0da292ab548d4709e
Emmanuel <dammylola299@gmail.com>
2023-07-25 03:52:07 +0100

    updates

--- app.js
+++ app.js
@@ -4,31 +4,34 @@ const express = require('express');
 // invoke the express function and store in the app variable
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 // get request from the homepage which is the root
 app.get('/', (req, res) => {
     // res.send('<h1>Hello, Universe!</h1>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 
 });
 
 // get request from the about page
 app.get('/about', (req, res) => {
     // res.send('<h1>About, Universe!</h1>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 
 });
 
-// redirect user
-app.get('/about-us', (req, res) => {
-    // res.send('<h1>About, Universe!</h1>');
-    res.redirect('/about');
-
+// get request from the create new-blog page
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 });
 
 // 404-error-page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
+
+
