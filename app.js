c53b8ebba3912a420e01d11e9644fc9b493fab55
Emmanuel <dammylola299@gmail.com>
2023-07-25 19:15:06 +0100

    updates

--- app.js
+++ app.js
@@ -12,26 +12,30 @@ app.listen(3000);
 
 // get request from the homepage which is the root
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello, Universe!</h1>');
-    res.render('index');
+    const blogs = [
+        { title: 'Mind the master', snippets: 'This is a very good compendium of James Allen best works' },
+        { title: 'Cant Hurt me', snippets: 'This is a very good piece by David Goggins' },
+        { title: 'Deep work', snippets: 'This is an excellent piece by Cal Newport' },
+    ];
+    res.render('index', { title: 'Home', blogs: blogs });
 
 });
 
 // get request from the about page
 app.get('/about', (req, res) => {
     // res.send('<h1>About, Universe!</h1>');
-    res.render('about');
+    res.render('about', { title: 'About' });
 
 });
 
 // get request from the create new-blog page
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create new blog' });
 });
 
 // 404-error-page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 });
 
 
